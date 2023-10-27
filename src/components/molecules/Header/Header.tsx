610af561dfe407497b6224e7783d1419f634cae4
johntagbo <johntagbo2@yahoo.com>
2023-10-27 16:36:50 +0100

    readme edited

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -2,7 +2,7 @@ import H1 from "../../atoms/H1";
 
 const Header = () => {
   return (
-    <header className="bg-dark px-10 py-5 mb-10">
+    <header className="bg-dark px-5 md:px-7 lg:px-10 py-2 md:py-5 mb-10">
       <H1 styling="text-white" text="Developer Test" />
     </header>
   );
